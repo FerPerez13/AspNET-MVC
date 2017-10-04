21eb1967f5d2ad98bb63f44912cc9fc042e29263
FerPerez13 <fer13vlc@gmail.com>
2017-10-04 09:46:53 +0200

    First Commit de ASP.NET

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            var peliculaService = new PeliculasService();
+            var model = peliculaService.ObtenerPeliculas();
+
+            return View(model);
+        }
+
+        // #### DIFERENTES FORMAS DE PASAR CONTENIDO A LAS PANTALLAS ####
+        // (Arriba tenemos la típica, un ActionResult enviando un View() vacío o lleno)
+
+        /* Pasar Contenido plano 
+        public ContentResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return Content("Fernando"); // También podemos poner HTML breve
+        }        
+        */
+
+        /* Enviar un JSON a la siguiente pantalla
+        public JsonResult Contact()
+        {
+            var pelicula = new Pelicula() {Titulo = "Batman Begins", Duracion = 180, Pais = "Estados Unidos" };
+            var pelicula2 = new Pelicula() { Titulo = "The Amazing Spiderman", Duracion = 150, Pais = "EE.UU" };
+            var pelicula3 = new Pelicula() { Titulo = "Wonder Woman", Duracion = 192, Pais = "Estados Unidos" };
+            var pelicula4 = new Pelicula() { Titulo = "Injustice", Duracion = 200, Pais = "Canada" };
+
+
+            return Json(new List<Pelicula>() { pelicula, pelicula2, pelicula3, pelicula4 }, JsonRequestBehavior.AllowGet); //Cuidado que tenemos que poner JsonRequestBehavior.AllowGet para que nos deje por Get 
+        }
+        */
+
+        /* Hacer una Redirección cuando pulsemos
+        public RedirectToRouteResult Contact()
+        {
+            //return Redirect("https://www.stackoverflow.com"); // (RedirectResult) Enviar a otra página fuera de nuestro sitio web
+            return RedirectToAction("Register", "Account"); // (RedirectToRouteResult) Hay que cambiar RedirectResult por RedirectToRouteResult
+        } 
+        */
+
+        /* Descargar un archivo desde la web
+        public FileResult Contact()
+        {
+            var ruta = Server.MapPath("/Sources/ejemplo.pdf"); // La ruta se puede poner con "/" o con "//"
+            return File(ruta, "application/pdf","ejemplo.pdf");
+        }
+        */
+
+        /* Parametros en un action y QueryString 
+        public ActionResult Contact(string nombre)
+        {
+            ViewBag.Message = "Your Contact Page." + nombre; // Lo tendremos que poner en la URL del navegador ?{Variable}={valor}
+            return View(); // No es recomendable para datos sensibles
+        }        
+        */
+
+        /* httpGet y httpPost 
+        [HttpGet] // Solo arrancará este trozo cuando hagamos un GET (Peticion de recibir info de web)
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your Contact Page.";
+            return View(); 
+        }
+
+        [HttpPost] // Solo arrancará este trozo cuando hagamos un POST (Envio de información a página)
+        public ActionResult Contact(int edad)
+        {
+            ViewBag.Message = "Your Contact Page. Your age is " + edad; 
+            return View(); 
+        }
+        */
+
+        /* ViewBag y ViewData */
+        public ActionResult Contact()
+        { //Los valores no se guardan en el servidor. Solo estarán disponibles para la vista que invocamos
+            ViewBag.Message = "Enviado con ViewBag";
+            ViewData["Mensaje"] = "Enviado con ViewData";
+            return View();
+        }
+        //*/
+
+        // ########## A continuación se hará todo lo referente a las Vistas ##########
+         public ActionResult MiAction()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ WebApplication1/Services/PeliculasService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.Services
+{
+    public class PeliculasService
+    {
+        public Pelicula ObtenerPelicula()
+        {
+            return new Pelicula()
+            {
+                Titulo = "Harry Potter",
+                Duracion = 115,
+                Pais = "USA",
+                Publicacion = new DateTime(2013, 12, 5)
+            };
+
+        }
+
+        public List<Pelicula> ObtenerPeliculas()
+        {
+            // TODO ESTO LO TENDRÍA QUE RECOGER DESDE UNA BASE DE DATOS
+            var pelicula1 = new Pelicula()
+            {
+                Titulo = "Harry Potter y la piedra filosofal",
+                Duracion = 115,
+                Pais = "USA",
+                Publicacion = new DateTime(2013, 12, 5)
+            };
+
+            var pelicula2 = new Pelicula()
+            {
+                Titulo = "Batman, el caballero oscuro",
+                Duracion = 213,
+                Pais = "UK",
+                Publicacion = new DateTime(2014, 11, 5)
+            };
+            // -----------------------------------------------------------
+
+            return new List<Pelicula> { pelicula1, pelicula2 }; // Devolvemos la lista de las peliculas
+        }
+    }
+}
